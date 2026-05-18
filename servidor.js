372e54fb3259ddc6e44dfa715c2aa4d2ddd0c50b
diegosein <diegui.sein@gmail.com>
2026-05-18 20:54:58 -0300

    Add files via upload

---
+++ servidor.js
@@ -0,0 +1,82 @@
+var http = require('http');
+var express = require('express');
+var colors = require('colors');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log('Servidor rodando ...'.rainbow);
+
+app.get('/', function (requisicao, resposta){
+resposta.redirect('home.html')
+})
+
+app.get('/inicio', function (requisicao, resposta){
+var nome = requisicao.query.info;
+console.log(nome);
+})
+
+app.post('/inicio', function (requisicao, resposta){
+var data = requisicao.body.data;
+console.log(data);
+})
+
+app.get('/cadastro',function (requisicao, resposta){
+var nome = requisicao.query.nome;
+var sobrenome = requisicao.query.sobrenome;
+var nascimento = requisicao.query.nascimento;
+var civil = requisicao.query.civil;
+
+resposta.render('resposta_cadastro', {nome, sobrenome, nascimento, civil})
+})
+
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+
+const uri = `mongodb+srv://diegosein:<db_password>@aula.px8rres.mongodb.net/?appName=aula`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
+
+app.post("/cadastrar_usuario", function(req, resp) {
+    var data = { db_nome: req.body.nome, db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.insertOne(data, function (err) {
+      if (err) {
+        console.log(err);
+        resp.render('resposta_usuario.ejs', {resposta: "Erro ao cadastrar usuário!"})
+      }else {
+        resp.render('resposta_usuario.ejs', {resposta: "Usuário cadastrado com sucesso!"})        
+      };
+    });
+   
+  });
+
+app.post("/logar_usuario", function(req, resp) {
+    var data = {db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.find(data).toArray(function(err, items) {
+      console.log(items);
+      if (items.length == 0) {
+        resp.render('resposta_usuario.ejs', {resposta: "Usuário/senha não encontrado!"})
+      }else if (err) {
+        resp.render('resposta_usuario.ejs', {resposta: "Erro ao logar usuário!"})
+      }else {
+        resp.render('resposta_usuario.ejs', {resposta: "Usuário logado com sucesso!"})        
+      };
+    });
+
+  }); 
